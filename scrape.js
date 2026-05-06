81f3e9eab9568b24babfb8c956dd4914c1746aaf
Artem Levchenko <globalizator@gmail.com>
2026-05-06 22:09:25 +0300

    Add web scraping script using Puppeteer
    This script uses Puppeteer to scrape a table from a specified VK page and saves it as 'schedule.html'.

---
+++ scrape.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+
+    await page.goto('https://vk.com/page-34729581_49195330', {
+        waitUntil: 'networkidle2',
+        timeout: 30000
+    });
+
+    await page.waitForSelector('table', { timeout: 15000 }).catch(() => {});
+
+    const tableHTML = await page.evaluate(() => {
+        const table = document.querySelector('table');
+        return table ? table.outerHTML : '';
+    });
+
+    await browser.close();
+
+    if (tableHTML) {
+        fs.writeFileSync('schedule.html', tableHTML);
+        console.log('Saved successfully');
+    } else {
+        console.log('Table not found');
+    }
+})();
